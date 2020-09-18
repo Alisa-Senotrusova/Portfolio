2b1ccdd45ffd1ef7e5b1ca4191e6f620f3ab3e32
Alise <a.senotrusova@gmail.com>
2020-09-18 23:50:45 +0800

    first commit

--- js/main.js
+++ js/main.js
@@ -21,3 +21,25 @@ $(document).ready(function () {
   });
 
 });
+
+$(document).ready(function () {
+
+  const formInputs = document.querySelectorAll('.form-field');
+
+  for(let item of formInputs){
+
+    const thisPlaceholder = item.nextElementSibling;
+
+    item.addEventListener('focus', function(){
+      thisPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function(){
+      if (item.value == '') {
+        thisPlaceholder.classList.remove('active');
+      }
+    });
+
+  }
+
+});
